180d14e5bf144062cc17ec2261bb8f85480f9df7
Yifei Li <xzco.work@gmail.com>
2024-10-19 10:44:19 +0800

    aggregate classifier

--- src/decisiontree/Classifier.java
+++ src/decisiontree/Classifier.java
@@ -1,8 +1,22 @@
 package decisiontree;
 
+import java.util.List;
+
 public class Classifier {
-    public static int classify(Object[] row, TreeNode node) {
+    public static void classify(List<Object[]> testData, TreeNode tree) {
+        int CorrectClassifiedNum = 0;
+        for (Object[] row : testData) {
+            int prediction = categorize(row, tree);
+            if (prediction == (Integer) row[row.length - 1]) {
+                CorrectClassifiedNum++;
+            }
+        }
+        System.out.println("CorrectClassifiedNum: " + CorrectClassifiedNum + ", Total: " + testData.size());
+        System.out.println("Precision: " + ((double) CorrectClassifiedNum / testData.size()));
+    }
+
+    public static int categorize(Object[] row, TreeNode node) {
         return (node instanceof LeafNode) ? ((LeafNode) node).category
-                : classify(row, node.condition.check(row) ? node.trueBranch : node.falseBranch);
+                : categorize(row, node.condition.check(row) ? node.trueBranch : node.falseBranch);
     }
 }
--- src/decisiontree/Main.java
+++ src/decisiontree/Main.java
@@ -3,7 +3,6 @@ package decisiontree;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import static decisiontree.Classifier.classify;
 import static decisiontree.DecisionTreeSaver.saveTreeToFile;
@@ -11,32 +10,30 @@ import static decisiontree.FilePreprocessor.processAndSaveFile;
 import static decisiontree.TreeBuilder.buildTree;
 
 public class Main {
+    static String trainingSetInputFilePath = "src/input/adult.data";
+    static String trainingSetOutputFilePath = "src/output/adult_preprocessed.csv";
+    static String testSetInputFilePath = "src/input/adult.test";
+    static String testSetOutputFilePath = "src/output/adult_test_preprocessed.csv";
+    static String decisionTreeFilePath = "src/output/decision_tree.txt";
+
     public static void main(String[] args) throws IOException {
-        // 1. Pre-process training set, change the file path params if necessary.
-        List<Object[]> trainingData = processAndSaveFile("src/input/adult.data", "src/output/adult_preprocessed.csv");
+        // 1. Pre-process training set.
+        long beginTime = System.currentTimeMillis();
+        List<Object[]> trainingData = processAndSaveFile(trainingSetInputFilePath, trainingSetOutputFilePath);
 
         // 2. Build decision tree based on training set data.
         System.out.println("Building decision tree will take several minutes...");
-        Set<Integer> usedCols = new HashSet<Integer>(); //初始化一个“使用过的列”
-        TreeNode tree = buildTree(trainingData, usedCols);
-        saveTreeToFile(tree, "src/output/decision_tree.txt");
-        System.out.println("Decision tree built, please check output directory.");
+        TreeNode decisionTree = buildTree(trainingData, new HashSet<Integer>());
+        saveTreeToFile(decisionTree, decisionTreeFilePath);
+        System.out.println("Decision tree built, please check the output directory.");
 
         // 3. Pre-process test set, change the file path params if necessary.
-        List<Object[]> testData = processAndSaveFile("src/input/adult.test", "src/output/adult_test_preprocessed.csv");
+        List<Object[]> testData = processAndSaveFile(testSetInputFilePath, testSetOutputFilePath);
 
         // 4. Classify test data.
         System.out.println("Classifying test data...");
-        int correct = 0;
-        for (Object[] row : testData) {
-            int prediction = classify(row, tree);
-            if (prediction == (Integer) row[row.length - 1]) {
-                correct++;
-            }
-        }
-
-        // 5. Print out precision rate.
-        System.out.println("Correct: " + correct + ", Total: " + testData.size());
-        System.out.println("Precision: " + ((double) correct / testData.size()));
+        classify(testData, decisionTree);
+        long endTime = System.currentTimeMillis();
+        System.out.println("Total Time: " + (endTime - beginTime) / 1000 + "s");
     }
 }
