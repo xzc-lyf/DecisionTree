15c4c9740c5deb900b8ab8c07f74d0329c8090ae
Yifei Li <xzco.work@gmail.com>
2024-10-20 00:13:49 +0800

    change to <=

--- src/decisiontree/Predicate.java
+++ src/decisiontree/Predicate.java
@@ -29,7 +29,7 @@ public class Predicate {
 
     @Override
     public String toString() {
-        return "Is " + attributes[attribute] + ((value instanceof Integer) ? " > " : " == ") + value + "?";
+        return "Is " + attributes[attribute] + ((value instanceof Integer) ? " <= " : " == ") + value + "?";
 
     }
 }
